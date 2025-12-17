4611bba161fa8121f9534b2ca76df738d72858cd
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2025-12-17 15:00:51 +0000

    Demo ysoserial CommonsCollections2 gadget chain

---
+++ src/main/java/org/dontpanic/ysoserial/poc/Main.java
@@ -0,0 +1,25 @@
+package org.dontpanic.ysoserial.poc;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        if (args.length < 1) {
+            System.out.println("Usage: java -jar ysoserial-poc.jar <file>");
+            return;
+        }
+
+        String filename = args[0];
+        System.out.printf("Reading file %s%n", filename);
+
+        try (InputStream is = new FileInputStream(filename)) {
+            ObjectInputStream ois = new ObjectInputStream(is);
+            Object obj = ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
